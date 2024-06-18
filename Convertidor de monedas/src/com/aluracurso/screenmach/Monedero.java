e9a448501f5e441eb860f34ff4d129f5806e2d8a
Stefhan <ingesonidoays@gmail.com>
2024-06-17 22:33:59 -0400

    Orden modificaciones
    Orden y modificaciones

---
+++ Convertidor de monedas/src/com/aluracurso/screenmach/Monedero.java
@@ -0,0 +1,62 @@
+package com.aluracurso.screenmach;
+
+public class Monedero {
+    String nombre;
+    private int fechaDeLanzamiento;
+    private int duracionEnMinutos;
+    private boolean incluidoEnElPlan;
+    private double sumaDeLasEvaluaciones;
+    private int totalDelasEvaluaciones;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public boolean isIncluidoEnElPlan() {
+        return incluidoEnElPlan;
+    }
+
+    public int getDuracionEnMinutos() {
+        return duracionEnMinutos;
+    }
+
+    public int getFechaDeLanzamiento() {
+        return fechaDeLanzamiento;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
+    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
+        this.incluidoEnElPlan = incluidoEnElPlan;
+    }
+
+    public int getTotalDelasEvaluaciones(){
+        return totalDelasEvaluaciones;
+    }
+
+    public void muestraFichaTecnica(){
+        System.out.println("El nombre de la película es: " + nombre);
+        System.out.println("Su fecha de lanzamiento es: " + fechaDeLanzamiento);
+        System.out.println("Duración en minutos: "+ duracionEnMinutos);
+    }
+
+    public void evalua(double nota){
+        sumaDeLasEvaluaciones += nota;
+        totalDelasEvaluaciones++;
+    }
+
+    public double calculaMedia(){
+        return sumaDeLasEvaluaciones / totalDelasEvaluaciones;
+    }
+}
+
